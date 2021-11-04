6f3e53d88c62b22f4a3552ca28d9a9e4070142be
Efrain Hernandez <efrainhg2050@gmail.com>
2021-11-04 10:10:53 -0600

    Card value function for deck

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -44,4 +44,17 @@ const pedirCarta = () => {
     
 }
 
-pedirCarta();
+//pedirCarta();
+
+
+const valorCarta = ( carta ) => {
+    const valor = carta.substring(0, carta.length - 1);
+    return ( isNaN(valor) )
+            ? (valor === 'A')
+                ? 11
+                : 10
+            : valor * 1;
+}
+
+const valor = valorCarta(pedirCarta());
+console.log({ valor });
